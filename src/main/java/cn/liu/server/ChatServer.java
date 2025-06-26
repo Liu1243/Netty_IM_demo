84ba85664bb293461dc6a51a6796da66b15c25fa
nI7_4DB <liu1243885596@gmail.com>
2025-06-26 23:16:29 +0800

    impl login

---
+++ src/main/java/cn/liu/server/ChatServer.java
@@ -0,0 +1,65 @@
+package cn.liu.server;
+
+import cn.liu.message.LoginRequestMessage;
+import cn.liu.message.LoginResponseMessage;
+import cn.liu.protocol.MessageCodecSharable;
+import cn.liu.protocol.ProcotolFrameDecoder;
+import cn.liu.server.service.UserServiceFactory;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ChatServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
+        MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new ProcotolFrameDecoder());
+                    ch.pipeline().addLast(LOGGING_HANDLER);
+                    ch.pipeline().addLast(MESSAGE_CODEC);
+                    // login
+                    ch.pipeline().addLast(new SimpleChannelInboundHandler<LoginRequestMessage>() {
+                        @Override
+                        protected void channelRead0(ChannelHandlerContext ctx, LoginRequestMessage msg) throws Exception {
+                            String username = msg.getUsername();
+                            String pwd = msg.getPassword();
+                            boolean login = UserServiceFactory.getUserService().login(username, pwd);
+                            LoginResponseMessage responseMsg;
+                            if (login) {
+                                responseMsg = new LoginResponseMessage(true, "登录成功");
+                            } else {
+                                responseMsg = new LoginResponseMessage(false, "用户名或密码错误");
+                            }
+
+                            ctx.writeAndFlush(responseMsg);
+                        }
+                    });
+                }
+            });
+            Channel channel = serverBootstrap.bind(8080).sync().channel();
+            channel.closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+}
